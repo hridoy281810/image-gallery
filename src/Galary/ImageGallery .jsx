c805cb797acadeb54c072f8245bf50e647b24964
hridoy281810 <hridoy01724281810@gmail.com>
2023-11-04 21:10:11 +0600

    i have create drag and droup customly

--- src/Galary/ImageGallery .jsx
+++ src/Galary/ImageGallery .jsx
@@ -16,77 +16,42 @@ import { IoCheckbox, IoImage } from "react-icons/io5";
 
 const ImageGallery = () => {
   const initialImages = [
-    { id: 1, image: image1 },
-    { id: 2, image: image2 },
-    { id: 3, image: image3 },
-    { id: 4, image: image4 },
-    { id: 5, image: image5 },
-    { id: 6, image: image6 },
-    { id: 7, image: image7 },
-    { id: 8, image: image8 },
-    { id: 9, image: image9 },
-    { id: 10, image: image10 },
-    { id: 11, image: image11 },
+    { id: 1, image: image1, scale: 1},
+    { id: 2, image: image2, scale: 1},
+    { id: 3, image: image3, scale: 1 },
+    { id: 4, image: image4, scale: 1 },
+    { id: 5, image: image5, scale: 1 },
+    { id: 6, image: image6, scale: 1 },
+    { id: 7, image: image7, scale: 1 },
+    { id: 8, image: image8, scale: 1 },
+    { id: 9, image: image9, scale: 1 },
+    { id: 10, image: image10, scale: 1 },
+    { id: 11, image: image11, scale: 1 },
   ];
 
   const [images, setImages] = useState(initialImages);
-  const [draggedImage, setDraggedImage] = useState(null);
-  const [draggedOverIndex, setDraggedOverIndex] = useState(null);
+  const [draggedIndex, setDraggedIndex] = useState(null);
   const [selectedImages, setSelectedImages] = useState([]);
-console.log(draggedImage)
-console.log(draggedOverIndex)
-  // Load selected images from local storage on component mount
-  useEffect(() => {
-    const selectedImagesFromLocalStorage = localStorage.getItem('selectedImages');
-    if (selectedImagesFromLocalStorage) {
-      setSelectedImages(JSON.parse(selectedImagesFromLocalStorage));
-    }
-  }, []);
-
-  // Save selected images to local storage whenever the selectedImages state changes
-  useEffect(() => {
-    localStorage.setItem('selectedImages', JSON.stringify(selectedImages));
-  }, [selectedImages]);
 
   const handleDragStart = (event, index) => {
     event.dataTransfer.setData('text/plain', index);
-    setDraggedImage(images[index]);
+    setDraggedIndex(index);
   };
 
   const handleDragOver = (event, index) => {
     event.preventDefault();
-    if (draggedImage) {
-      const draggedIndex = images.indexOf(draggedImage);
-      if (index !== draggedIndex) {
-        setDraggedOverIndex(index);
-      }
+    if (draggedIndex !== null) {
+      const newImages = [...images];
+      const draggedImage = newImages[draggedIndex];
+      newImages.splice(draggedIndex, 1); // Remove the dragged item
+      newImages.splice(index, 0, draggedImage); // Insert it at the new position
+      setImages(newImages);
+      setDraggedIndex(index);
     }
   };
 
   const handleDragEnd = () => {
-    setDraggedImage(null);
-    setDraggedOverIndex(null);
-  };
-
-  const handleDrop = (event) => {
-    event.preventDefault();
-    if (draggedImage !== null && draggedOverIndex !== null) {
-      const newImages = [...images];
-      const draggedIndex = images.indexOf(draggedImage);
-
-      // Calculate the position to insert the dragged image
-      const insertionIndex = draggedOverIndex < draggedIndex ? draggedOverIndex : draggedOverIndex + 1;
-
-      // Remove the dragged image from its original position
-      newImages.splice(draggedIndex, 1);
-
-      // Insert the dragged image at the calculated position
-      newImages.splice(insertionIndex, 0, draggedImage);
-
-      setImages(newImages);
-      setDraggedImage(null);
-      setDraggedOverIndex(null);
-    }
+    setDraggedIndex(null);
   };
 
   const handleImageSelection = (id) => {
@@ -101,23 +66,25 @@ console.log(draggedOverIndex)
     const newImages = images.filter((image) => !selectedImages.includes(image.id));
     setImages(newImages);
     setSelectedImages([]);
-
-    // Remove selected images from local storage
-    localStorage.removeItem('selectedImages');
   };
-
   return (
     <div className="bg-gray-300 w-full p-4">
       <div className="bg-white rounded-tr-lg rounded-tl-lg px-6 py-4 border-b border-gray-400">
-        
         <div className="flex justify-between text-xl font-semibold">
           {selectedImages.length > 0 ? (
             <>
-              <div className='inline-flex justify-center items-center gap-2 '>
-                <span className='text-blue-600 '><IoCheckbox /></span>
+              <div className="inline-flex justify-center items-center gap-2">
+                <span className="text-blue-600">
+                  <IoCheckbox />
+                </span>
                 <p>{selectedImages.length} Files Selected</p>
               </div>
-              <p onClick={handleDeleteSelectedImages} className='text-red-600 cursor-pointer'>Delete</p>
+              <p
+                onClick={handleDeleteSelectedImages}
+                className="text-red-600 cursor-pointer"
+              >
+                Delete
+              </p>
             </>
           ) : (
             <p>Gallery</p>
@@ -126,28 +93,27 @@ console.log(draggedOverIndex)
       </div>
 
       <div className="bg-white rounded-br-lg rounded-bl-lg p-4">
-        <div  className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4" onDrop={handleDrop} onDragOver={(e) => e.preventDefault()}>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
           {images.map(({ image, id }, index) => (
             <div
-            key={id}
-            className={`relative cursor-pointer border-2 border-gray-400 rounded-lg  overflow-hidden hover:border-none  ${
-                index === 0 ? 'col-span-2 md:row-span-2 lg:row-span-2' : ''
+              key={id}
+              className={`relative cursor-pointer border-2 border-gray-400 rounded-lg overflow-hidden hover:border-none   ${
+                index === 0 ? 'col-span-2 md:row-span-2 lg:row-span-2' : 'col-span-1'
               }`}
-            onDragOver={(event) => handleDragOver(event, index)}
-            onDragStart={(event) => handleDragStart(event, index)}
-            onDragEnd={handleDragEnd}
-            draggable
+              onDragOver={(event) => handleDragOver(event, index)}
+              onDragStart={(event) => handleDragStart(event, index)}
+              onDragEnd={handleDragEnd}
+              draggable
             >
+              {/* Change the image size based on the drag state */}
               <img
                 src={image}
-                alt={`Image ${index + 1}`}
-                className={`w-full h-auto transition-transform duration-500 ease-in-out ${
-                  draggedImage && index === draggedOverIndex
-                    ? 'transform scale-105 opacity-100 rounded-lg z-10   '
-                    : 'transform hover:scale-110  duration-300 opacity-100'
+                alt={`Image $\b{index + 1}`}
+                className={`w-full h-auto transition-transform duration-300 ${
+                  index === draggedIndex ? 'transform scale-110' : ''
                 }`}
               />
-    <div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-700 opacity-0 hover:opacity-50 flex items-center justify-center rounded-lg border-2 border-gray-500" >
+              <div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-700 opacity-0 hover:opacity-50 flex items-center justify-center rounded-lg border-2 border-gray-500">
                 <input
                   type="checkbox"
                   className={`checkbox-primary mr-1 absolute top-4 left-4 z-10`}
@@ -156,17 +122,10 @@ console.log(draggedOverIndex)
               </div>
             </div>
           ))}
-          <div className=' flex flex-col justify-center items-center cursor-pointer border-2  border-dashed   border-gray-400 rounded-lg text-gray-500 ' >
-              <IoImage size={30} />
-              <p className='text-xl mt-4'>
-                Add Images
-              </p>
-          </div>
         </div>
       </div>
-
     </div>
   );
 };
 
-export default ImageGallery
+export default ImageGallery;
